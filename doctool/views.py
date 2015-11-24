f1f90721b01fcbfb5675f22d388c5c58349de0e0
MrLYC <imyikong@gmail.com>
2015-11-24 10:09:20 +0800

    update

--- doctool/views.py
+++ doctool/views.py
@@ -1,7 +1,18 @@
 import json
+import os
+import re
 
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+EmptyLineRex = re.compile(r"^(\s*\n){3}", re.M | re.S)
 
 
 def render_doc(request):
-    return render(request, "doc.html", json.loads(request.body))
+    result = os.linesep.join(
+        i.rstrip()
+        for i in EmptyLineRex.split(
+            render_to_string("doc.html", json.loads(request.body))
+        )
+    )
+    return HttpResponse(result)
